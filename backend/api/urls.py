cfc49e42f8488e214177348e0807a32e3a38a174
Вадим Шпак <starboyshpak@yandex.ru>
2023-03-03 05:14:50 +0300

    fix: pep8 after update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,20 +18,3 @@ urlpatterns = [
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
-
-app_name = 'api'
-
-
-
-
-
-
-# from foodgram.urls import urlpatterns as foodgram
-# from users.urls import urlpatterns as users
-
-# app_name = 'api'
-
-# urlpatterns = []
-
-# urlpatterns += users
-# urlpatterns += foodgram
