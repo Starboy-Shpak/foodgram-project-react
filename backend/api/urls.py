e5380e44f614c7689014cbd99c8d44bac23551d3
Вадим Шпак <starboyshpak@yandex.ru>
2023-03-04 05:06:22 +0300

    test: test 3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (FoodgramUsersViewSet, IngredintViewSet,
+from .views import (CustomUserViewSet, IngredientViewSet,
                     RecipeViewSet, TagViewSet)
 
 app_name = 'api'
 
 
 router = DefaultRouter()
-router.register(r'users', FoodgramUsersViewSet, basename='users')
-router.register(r'ingredients', IngredintViewSet, basename='ingredients')
+router.register(r'users', CustomUserViewSet, basename='users')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'recipes', RecipeViewSet, basename='recipes')
 router.register(r'tags', TagViewSet, basename='tags')
 
