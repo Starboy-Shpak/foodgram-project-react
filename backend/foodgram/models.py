7154db74b4c0efce2b08b9526e54786087864a18
Вадим Шпак <starboyshpak@yandex.ru>
2023-03-05 05:46:24 +0300

    update: recipe ordering

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -60,7 +60,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
-        ordering = ('name',)
+        ordering = ('-pub_date',)
 
     def __str__(self) -> str:
         return self.name
